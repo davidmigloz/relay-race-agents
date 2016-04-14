c6b137ef6da6a9a28ffc47f7a1cd1a04ad488143
davidmigloz <me@davidmiguel.com>
2016-04-14 20:25:24 +0200

    Implement ExperimentBehaviour

--- src/main/java/com/davidmiguel/relayrace/agents/JudgeAgent.java
+++ src/main/java/com/davidmiguel/relayrace/agents/JudgeAgent.java
@@ -3,14 +3,31 @@ package com.davidmiguel.relayrace.agents;
 import com.davidmiguel.relayrace.behaviours.ExperimentBehaviour;
 
 import jade.core.Agent;
+import jade.util.Logger;
 
 public class JudgeAgent extends Agent {
 
+	private final Logger logger = Logger.getMyLogger(getClass().getName());
 	private static final long serialVersionUID = 934303410329286008L;
 
+	private int numAttempts;
+	private int initLaps;
+	private int step;
+
 	@Override
-	protected void setup() {		
+	protected void setup() {
+		// Get arguments (nAttempts, initLaps, step)
+		Object[] args = getArguments();
+		if (args != null && args.length == 3) {
+			numAttempts = Integer.parseInt((String) args[0]);
+			initLaps = Integer.parseInt((String) args[1]);
+			step = Integer.parseInt((String) args[2]);
+			logger.info("Init experiment (a:" + numAttempts + ", i:" + initLaps + ", s:" + step);
+		} else {
+			logger.log(Logger.SEVERE, "Agent " + getLocalName() + " - Incorrect number of arguments");
+			doDelete();
+		}
 		// Init experiment
-		addBehaviour(new ExperimentBehaviour());
+		addBehaviour(new ExperimentBehaviour(numAttempts, initLaps, step));
 	}
 }
--- src/main/java/com/davidmiguel/relayrace/behaviours/ExperimentBehaviour.java
+++ src/main/java/com/davidmiguel/relayrace/behaviours/ExperimentBehaviour.java
@@ -1,5 +1,12 @@
 package com.davidmiguel.relayrace.behaviours;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 import com.davidmiguel.relayrace.utils.AgentsUtils;
 
 import jade.core.behaviours.SimpleBehaviour;
@@ -7,42 +14,118 @@ import jade.domain.DFService;
 import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 import jade.util.Logger;
 
 public class ExperimentBehaviour extends SimpleBehaviour {
 
 	private final Logger logger = Logger.getMyLogger(getClass().getName());
 	private static final long serialVersionUID = 6100703780980004688L;
-	
+
 	/** Delay to start the experiment (in seconds) */
 	private static final int DELAY = 10;
-	
+
+	private JSONArray results;
 	private int numTeams;
 	private int numMachines;
 	private DFAgentDescription[] captains;
+	private long startTime;
+	private long endTime;
+	private int numLaps;
+	private int attempt;
+	private int numMaxAttempts;
+	private int step;
+	private MessageTemplate iMT;
+	private MessageTemplate cMT;
+	private int teamsConfirmed;
 
+	public ExperimentBehaviour(int numAttempts, int initLaps, int step) {
+		this.results = new JSONArray();
+		this.attempt = 1;
+		this.numLaps = initLaps;
+		this.numMaxAttempts = numAttempts;
+		this.step = step;
+		this.iMT = MessageTemplate.MatchConversationId("init");
+		this.cMT = MessageTemplate.MatchConversationId("completion");
+	}
+
+	@SuppressWarnings("unchecked")
 	@Override
 	public void action() {
 		try {
 			// Get captains of all teams (RAc)
 			captains = DFService.search(myAgent, AgentsUtils.getDFD(AgentsUtils.getSD("RAc")));
-			
+			// Count number of teams
+			numTeams = captains.length;
+			// Get rest of members of the teams (RA)
+			DFAgentDescription[] runners = DFService.search(myAgent, AgentsUtils.getDFD(AgentsUtils.getSD("RA")));
+			// Count number of machines (one member per machine)
+			numMachines = runners.length / numTeams;
 		} catch (FIPAException e) {
 			logger.log(Logger.SEVERE, "Cannot get runners", e);
 		}
-		// Send START_TIME message to all runners
+		// Send start time and number of laps to team captains
 		ACLMessage startMsg = new ACLMessage(ACLMessage.REQUEST);
+		startMsg.setConversationId("init");
 		for (int i = 0; i < captains.length; ++i) {
 			startMsg.addReceiver(captains[i].getName());
 		}
-		long startTime = System.currentTimeMillis() + DELAY * 1000;
-		startMsg.setContent(startTime + "");
+		startTime = System.currentTimeMillis() + DELAY * 1000;
+		startMsg.setContent(startTime + ";" + numLaps);
 		myAgent.send(startMsg);
-		logger.info("Start time sent!");
+		logger.info("Start msg sent!");
+		// Receive confirmations
+		teamsConfirmed = 0;
+		while (teamsConfirmed != numTeams) {
+			ACLMessage confMsg = this.getAgent().receive(iMT);
+			if (confMsg != null) {
+				teamsConfirmed++;
+				logger.info("Teams confirmed: " + teamsConfirmed + "/" + numTeams);
+			}
+		}
+		// Receive completion confirmations
+		teamsConfirmed = 0;
+		while (teamsConfirmed != numTeams) {
+			ACLMessage compMsg = this.getAgent().receive(cMT);
+			if (compMsg != null) {
+				teamsConfirmed++;
+				logger.info("Teams finished: " + teamsConfirmed + "/" + numTeams);
+			}
+		}
+		// Get end time
+		endTime = System.currentTimeMillis();
+		// Save results
+		JSONObject attResults = new JSONObject();
+		attResults.put("machines", numMachines);
+		attResults.put("teams", numTeams);
+		attResults.put("laps", numLaps);
+		attResults.put("runtime", endTime - startTime);
+		results.add(attResults);
+		// Update variables
+		numLaps += step;
+		attempt++;
 	}
 
 	@Override
 	public boolean done() {
-		return false;
+		return attempt > numMaxAttempts;
+	}
+
+	@Override
+	public int onEnd() {
+		// Save results to file
+		try {
+			File file = new File("result.json");
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+			FileWriter fileWritter = new FileWriter(file.getName(), true);
+			fileWritter.write(results.toJSONString());
+			fileWritter.flush();
+			fileWritter.close();
+		} catch (IOException e) {
+			logger.log(Logger.SEVERE, "Cannot write results to file");
+		}
+		return 0;
 	}
 }
